265d0c511051447221e0d8d058335b13637a2730
aritra <m1999.aritra@gmail.com>
2023-09-14 15:06:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Aritra
         </a>
       </header>
     </div>
